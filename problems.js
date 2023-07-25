4828e321cd9297badde6f8fb53b859a9040d4d0c
emonh0que <you@example.com>
2023-07-25 21:34:32 +0600

    completed task two

--- problems.js
+++ problems.js
@@ -1,10 +1,26 @@
+// in this function we will reverse the input string
 const reverseString = text => {
-    const textLength = text.length;
-    let newText = '';
+    const textLength = text.length; // calculating length of the string so that we could loop from the end
+    let newText = ''; // here we will store the reveresed text
     for(let i = textLength - 1; i >= 0; i--){
        newText += text[i];
     }
-    return newText;
+    return newText; // returning the reveresd string
 }
 
-console.log(reverseString('hello world'));
+// console.log(reverseString('hello world'));
+
+
+// In this function we will take an array and only return the sum of all positive integers
+const sumPositive = arr => {
+    let sum = 0; // here we will store the value of all positive integers
+    for(let i of arr){
+       if(i >= 0) {
+        sum += i;
+       }
+
+    }
+    return sum;
+}   
+
+// console.log(sumPositive([2, -5, 10, -3, 7]));
